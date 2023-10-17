2c0f9bf3ff3fa5b2141c4c737eee946c35744e13
anis_k <aanniissk@gmail.com>
2023-10-17 17:46:52 -0500

    cyka

--- src/test/java/com/demro/steps/US01.java
+++ src/test/java/com/demro/steps/US01.java
@@ -4,8 +4,15 @@ import com.demro.utility.DB_Util;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import java.sql.ResultSet;
+import java.util.List;
 
 public class US01 {
+    ResultSet Rs;
+    int Cyka1;
+    int Cyka2;
     @Given("Establish the database connection")
     public void establish_the_database_connection() {
         DB_Util.createConnection();
@@ -13,17 +20,24 @@ public class US01 {
     }
     @When("Execute query to get all IDs from users")
     public void execute_query_to_get_all_i_ds_from_users() {
+        DB_Util.runQuery("select id from users");
     }
     @Then("verify all users has unique ID")
     public void verify_all_users_has_unique_id() {
+        Cyka1 = DB_Util.getColumnCount();
+        DB_Util.runQuery("select distinct id from users");
+        Cyka2 = DB_Util.getColumnCount();
+        Assert.assertEquals(Cyka1,Cyka2);
+
     }
 
     @When("Execute query to get all columns")
     public void execute_query_to_get_all_columns() {
+       Rs= DB_Util.runQuery("select * from users");
     }
 
     @Then("verify the below columns are listed in result")
-    public void verify_the_below_columns_are_listed_in_result(io.cucumber.datatable.DataTable dataTable) {
-
+    public void verify_the_below_columns_are_listed_in_result(List<String>tchshit) {
+        Assert.assertTrue(tchshit.equals(DB_Util.getAllColumnNamesAsList()));
     }
 }
